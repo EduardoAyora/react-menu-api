88c20340dba0808b6df250d5d6096ecacc18f850
EduardoAyora <eduardoaayora64@gmail.com>
2020-10-14 18:11:11 -0500

    cors

--- routes/example.js
+++ routes/example.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const exampleRouter = express.Router()
 
-module.exports = exampleRouter.get('/', async (req, res) => {
+exampleRouter.get('/', async (req, res) => {
     try {
         const ex = {
             nombre: 'edu'
@@ -11,3 +11,28 @@ module.exports = exampleRouter.get('/', async (req, res) => {
         res.status(500).json({message: err.message})
     }
 })
+
+exampleRouter.put('/', async (req, res) => {
+    try {
+        const ex = {
+            nombre: req.body.nombre
+        }
+        res.json(ex)
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+exampleRouter.delete('/:id', async (req, res) => {
+    console.log(req.params.id)
+    try {
+        const ex = {
+            nombre: req.params.id
+        }
+        res.json(ex)
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+module.exports = exampleRouter
--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const cors = require('cors')
+// const io = require('socket.io')(http)
 
 const exampleRouter = require('./routes/example')
+
+var corsOptions = {
+    origin: 'http://localhost:4000',
+    optionsSuccessStatus: 200
+}
+
+// habilita o deshabilita los metodos diferentes de get, head, post
+app.options('*', cors())
+// habilita el dominio, puerto o protocolo
+app.use(cors(corsOptions))
+app.use(express.json())
 
 // rutas de la api
 app.use('/', exampleRouter)
 
-io.on('connection', (socket) => {
-    console.log('a user connected')
-});
+// io.on('connection', (socket) => {
+//     console.log('a user connected')
+// });
 
 http.listen(1100, () => {
     console.log('escuchando en :1100')
